79f8247288b7abcbcd2c2ae1a1f61375dd0676ce
pyk707 <pyk707@hanmail.net>
2021-01-21 23:15:15 +0900

    210121 upload

--- ch4.R
+++ ch4.R
@@ -1,4 +1,4 @@
-##ch4-1
+##ch4
 
 #열은 컬럼, 변수라고 부른다
 #행은 로(row), 케이스라고 부른다
@@ -11,7 +11,7 @@ class <- c(1,1,2,2)
 df_midterm <- data.frame(english, math, class)
 df_midterm
 
-#$사용법
+#'$'사용법 <- 열에 대해서 사용용
 mean(df_midterm$english)
 mean(df_midterm$math)
 
@@ -26,3 +26,13 @@ df_fruites = data.frame(fruit = c('사과', '딸기', '수박'),
                         price = c(1800, 1500, 3000),
                         amount = c(28, 34, 13))
 df_fruites
+
+
+#외부 데이터 이용하기
+
+install.packages("readxl")
+library(readxl)
+
+#df_exam <- read_excel("d:/easy_r/excel_exam.xlsx") ← 다른 경로의 파일은 이런 식으로
+df_exam = read_excel("excel_exam.xlsx")
+df_exam
