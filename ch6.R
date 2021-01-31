6e163af8b545b9e0b9ff147661285ba043152574
pyk707 <pyk707@hanmail.net>
2021-01-31 21:38:33 +0900

    210131 upload

--- ch6.R
+++ ch6.R
@@ -91,4 +91,34 @@ mpg %>%
 
 
 #데이터 합치기
+#가로로 합치기
+test1 <- data.frame(id = c(1,2,3,4,5),
+                    midterm = c(60,80,70,90,85))
+test2 <- data.frame(id = c(1,2,3,4,5),
+                    final = c(70,83,65,95,80))
+test1
+test2
 
+total_left <- left_join(test1,test2, by = 'id')
+total_left
+
+name <- data.frame(class = c(1, 2, 3, 4, 5),
+                   teacher = c("kim", "lee", "park", "choi", "jung"))
+name
+exam
+
+exam_new <- left_join(exam, name, by = "class")
+exam_new
+
+#세로로 합치기
+# 학생 1~5번 시험 데이터 생성
+group_a <- data.frame(id = c(1, 2, 3, 4, 5),
+                      test = c(60, 80, 70, 90, 85))
+# 학생 6~10번 시험 데이터 생성
+group_b <- data.frame(id = c(6, 7, 8, 9, 10),
+                      test = c(70, 83, 65, 95, 80))
+group_a
+group_b
+
+group_all <- bind_rows(group_a, group_b)
+group_all
