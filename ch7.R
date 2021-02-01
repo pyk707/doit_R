e41a088701b7b717646e6344c5d86e0da564b446
pyk707 <pyk707@hanmail.net>
2021-02-01 21:04:09 +0900

    210201 upload

--- ch7.R
+++ ch7.R
@@ -1,2 +1,18 @@
 #데이터 정제(빠진 데잍, 이상한 데이터 제거하기)
 #p164
+
+
+df <- data.frame(sex = c("M", "F", NA, "M", "F"),
+                 score = c(5, 4, 3, 4, NA))
+df
+
+library(dplyr)
+
+is.na(df)
+table(is.na(df))
+
+table(is.na(df$sex))
+table(is.na(df$score))
+
+df %>% 
+  filter(is.na(score))
